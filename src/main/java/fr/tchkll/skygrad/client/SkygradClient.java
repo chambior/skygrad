ef26fe3da33680667cb49af23a367f4239f74dd1
chambior <remi@potirouilles.com>
2026-05-08 21:29:19 +0200

    Fixes sky motor rendering

---
+++ src/main/java/fr/tchkll/skygrad/client/SkygradClient.java
@@ -0,0 +1,18 @@
+package fr.tchkll.skygrad.client;
+
+import dev.simulated_team.simulated.content.blocks.portable_engine.PortableEngineRenderer;
+import fr.tchkll.skygrad.ModBlockEntities;
+import fr.tchkll.skygrad.Skygrad;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+
+@EventBusSubscriber(modid = Skygrad.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+public class SkygradClient {
+
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(ModBlockEntities.SKY_ENGINE_BE.get(), PortableEngineRenderer::new);
+    }
+}
